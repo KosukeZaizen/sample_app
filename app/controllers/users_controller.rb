e2dabf3595126b1e34afa2fadd90c1d7c7baff01
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-02-03 11:11:33 +0000

    10.4.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
 
 
@@ -44,6 +45,15 @@ class UsersController < ApplicationController
   end
 
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+
+
+
   private
 
     def user_params
@@ -70,5 +80,10 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
